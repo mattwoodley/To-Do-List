cf76acbbd5aa593d9d04272cc1171829e6adf851
Matthew Woodley <matt@mattwoodley.net>
2020-04-02 16:32:56 +0100

    +deleteToDo +listContainer click evt handler

--- app.js
+++ app.js
@@ -18,29 +18,25 @@ const dateOptions = { weekday: "long", month: "short", day:"numeric"};
 date.innerHTML = today.toLocaleDateString("en-GB", dateOptions);
 
 addToDo = (id, toDo, completed, deleted) => {
-
     // check if deleted === true, if so then don't run code below.
     if (deleted) { return; }
 
     //  check completed state to decide what css styles to apply
-    let check = ""
-    let line = ""
+    let [completedCheck, lineThrough] = ["", ""];
     if (completed) {
-        check = "fa-check-circle"
-        line = "lineThrough"
+        completedCheck = "fa-check-circle"
+        lineThrough = "lineThrough"
     } else {
-        check = "fa-circle"
+        completedCheck = "fa-circle"
     }
-
-    newItem = `
-        <li class="item">
-            <i class="far ${check}" job="complete" id="${id}"></i>
-            <p class="text ${line}">${toDo}</p>
+    newToDo = `
+        <li class="toDo">
+            <i class="far ${completedCheck}" job="complete" id="${id}"></i>
+            <p class="text ${lineThrough}">${toDo}</p>
             <i class="fa fa-trash" job="delete" id="${id}"></i>
         </li>
     `
-
-    listContainer.insertAdjacentHTML("beforeend", newItem)
+    listContainer.insertAdjacentHTML("beforeend", newToDo)
 }
 
 // enter keyup to submit new toDo onto list
@@ -51,19 +47,41 @@ document.addEventListener("keyup", (evt) => {
         // check if toDo is empty
         if(toDo.length !== 0) {
             addToDo(id, toDo, false, false);
-
             list.push({
-                name: toDo,
+                toDoName: toDo,
                 id: id,
                 completed: false,
                 deleted: false
             });
-
             id++;
-
         } else {
-            throw "Error: To Do contains zero characters."
+            throw "Error: To Do cannot contain zero characters."
         }
         input.value = "";
     }
 });
+
+// delete to do
+
+deleteToDo = (toDo) => {
+    toDo.parentNode.parentNode.removeChild(toDo.parentNode);
+    list[toDo.id].deleted = true;
+}
+
+listContainer.addEventListener("click", (evt) => {
+    let toDoContainer = evt.target;
+    let toDoJob = null;
+    if (toDoContainer.hasAttribute("job")) {
+        toDoJob = toDoContainer.attributes.job.value; //complete or delete
+        if (toDoJob === "complete") {
+            completeToDo(toDoContainer);
+        } else if (toDoJob === "delete") {
+            deleteToDo(toDoContainer);
+        } else {
+            return;
+        }
+    }
+});
+
+addToDo(0, "Hello World", false, false);
+addToDo(1, "True", false, false);
